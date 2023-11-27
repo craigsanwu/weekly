e52f95816b32b9db3b7e99cbd2512ae1b2c40de5
dCOMMICS <sanwurld@gmail.com>
2023-11-27 15:43:37 +0300

    values and variables in js

--- fundamental pt1/index.js
+++ fundamental pt1/index.js
@@ -1,7 +1,20 @@
 let js = 'Amazing';
 
-        if (js=== 'Amazing'); alert ("Kayode");
+        // if (js=== 'Amazing'); alert ("Kayode");
 
-        40 + 15 + 37 + 18;
+        // 40 + 15 + 37 + 18;
 
-        console.log(40+15+37+18);
+        // console.log(40+15+37+18);
+
+        console.log("Ibrahim Kayode")
+
+
+let firstName = "Kaim";
+
+let last_name = " Christian";
+
+console.log(firstName + " " + last_name);
+
+let myfirst_job = " Illegal Hacker";
+
+let mycurrent_job = " Indie Hacker ";
