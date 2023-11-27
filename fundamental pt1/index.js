2d4442ff83dfa4a5207541452d59364614cb413e
dCOMMICS <sanwurld@gmail.com>
2023-11-27 15:48:46 +0300

    Fundamentals Part 1 Assignment

--- fundamental pt1/index.js
+++ fundamental pt1/index.js
@@ -18,3 +18,24 @@ console.log(firstName + " " + last_name);
 let myfirst_job = " Illegal Hacker";
 
 let mycurrent_job = " Indie Hacker ";
+
+
+// JavaScript Fundamentals – Part 1
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+
+
+let my_country = "South Africa";
+
+let my_continent = "Africa";
+
+let my_population = '59.39 Million';
+
+console.log(my_country);
+
+console.log(my_population)
+
+console.log(my_continent)
