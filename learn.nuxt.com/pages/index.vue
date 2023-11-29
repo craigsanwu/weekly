1d669fb8c58b65f4c5bc0f5cd26d03f619ff9286
dCOMMICS <sanwurld@gmail.com>
2023-11-29 20:14:13 +0300

    error on running files

---
+++ learn.nuxt.com/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps <{
+    modelValue?: boolean,
+}> ()
+</script>
+
+<template>
+    <div>
+        hello world
+        <slot/>
+    </div>
+</template>
