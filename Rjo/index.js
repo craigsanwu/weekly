002392b5fb0267c2f70ff674dadd91b4f798e4ff
dCOMMICS <sanwurld@gmail.com>
2023-12-06 12:57:30 +0300

    👍

--- Rjo/index.js
+++ Rjo/index.js
@@ -1,1 +1,14 @@
 console.log("Check online")
+
+
+// cursor position
+
+let page1Content = document.querySelector(".page1-content");
+
+let cursor = document.querySelector(".cursor");
+
+page1Content.addEventListener("mousemove",function(dets){
+    // console.log(dets.x)
+    cursor.style.left = dets.x + "px";
+    cursor.style.top = dets.y + "px";
+})
