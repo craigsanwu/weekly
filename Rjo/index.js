7fec2a646e08fe1c59636fbbbb5b6df597f659b4
dCOMMICS <sanwurld@gmail.com>
2023-12-06 13:04:10 +0300

    👍

--- Rjo/index.js
+++ Rjo/index.js
@@ -7,8 +7,12 @@ let page1Content = document.querySelector(".page1-content");
 
 let cursor = document.querySelector(".cursor");
 
-page1Content.addEventListener("mousemove",function(dets){
-    // console.log(dets.x)
-    cursor.style.left = dets.x + "px";
-    cursor.style.top = dets.y + "px";
+// page1Content.addEventListener("mousemove",function(dets){
+//  
+//     cursor.style.left = dets.x + "px";
+//     cursor.style.top = dets.y + "px";
+// })
+
+gsap.to(".cursor",{
+    x:500.
 })
