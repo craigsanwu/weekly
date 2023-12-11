bad569685fd470fe424e089d7de7994f6a02bf04
dCOMMICS <sanwurld@gmail.com>
2023-12-11 19:39:06 +0300

    portfolio website

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,21 @@ export default {
 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        buttonBgColor: "#0067FF",
+        yellowColor: "#FEB60D",
+        purpleColor: "#9771FF",
+        irisBlueColor: "#01B5C5",
+        headingColor: "#181a1e",
+        textColor: "#4e5455f",
+      },
+boxShadow:{
+  panelShadow:"rgba(17,12,46,0.15) 0px 48px 100px 0px;",
+
+},
+
+    },
   },
   plugins: [],
 }
