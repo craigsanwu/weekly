79795f3ecc1c2550480c99bf035db015231f1f5b
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-12-30 01:01:53 +0300

    initial commit

--- fundamental pt1/index.sql
+++ fundamental pt1/index.sql
@@ -4,3 +4,20 @@ SELECT
 FROM Activity
 WHERE activity_date BETWEEN '2019-06-28' AND  '2019-07-27'
 GROUP BY 1;
+
+-- question 1070 
+
+WITH
+  ProductToYear AS (
+    SELECT product_id, MIN(year) AS year
+    FROM Sales
+    GROUP BY 1
+  )
+SELECT
+  Sales.product_id,
+  ProductToYear.year AS first_year,
+  Sales.quantity,
+  Sales.price
+FROM Sales
+INNER JOIN ProductToYear
+  USING (product_id, year);
