c65d0b6d15ef668bea736bc32628d360c3ae6201
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-12-30 00:58:48 +0300

    solution to two sum quiz line 22

--- fundamental pt1/l.py
+++ fundamental pt1/l.py
@@ -16,3 +16,15 @@ class Solution:
           dp[i][k] = min(dp[i][k], dp[j][k - 1] + maxDifficulty)
 
     return dp[n][d]
+
+
+# two sum
+
+    class Solution:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    numToIndex = {}
+
+    for i, num in enumerate(nums):
+      if target - num in numToIndex:
+        return numToIndex[target - num], i
+      numToIndex[num] = i
