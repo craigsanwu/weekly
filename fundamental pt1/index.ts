189291df22ba9505b8b7ed0a4654b90e3845544b
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-12-30 01:25:23 +0300

    call function solution

--- fundamental pt1/index.ts
+++ fundamental pt1/index.ts
@@ -119,3 +119,34 @@ function cancellable<T>(
   
     return [cancel, promise];
   }
+
+
+//   2693 sol
+
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+
+declare global {
+  interface Function {
+    callPolyfill(
+      context: Record<string, JSONValue>,
+      ...args: JSONValue[]
+    ): JSONValue;
+  }
+}
+
+Function.prototype.callPolyfill = function (context, ...args): JSONValue {
+  const fn = this;
+  Object.defineProperty(context, '__fn__', {
+    value: fn,
+    enumerable: false,
+  });
+  const ans = (context.__fn__ as any)(...args);
+  delete context.__fn__;
+  return typeof ans !== 'undefined' ? ans : undefined;
+};
