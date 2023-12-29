c971c480db5346d3830db4a59827b892a7b6a0d3
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-12-30 01:16:58 +0300

    initial commit

--- fundamental pt1/index.ts
+++ fundamental pt1/index.ts
@@ -63,3 +63,16 @@ function memoize(fn: Fn): Fn {
     return ans;
   };
 }
+
+
+// 2648 sol
+
+function* fibGenerator(): Generator<number, any, number> {
+    const arr = [0, 0, 1];
+    while (true) {
+      yield arr[1];
+      arr[0] = arr[1];
+      arr[1] = arr[2];
+      arr[2] = arr[0] + arr[1];
+    }
+  }
