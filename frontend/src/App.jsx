7ddf383a799d060924bbc42f3533178f32dff578
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2024-01-02 14:24:37 +0300

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text=[20px] text-blue-600">Welcome to React + Vite</h1>
+      <h1 className="text=[20px] text-blue-600">Blue Lab Studio</h1>
     </>
   )
 }
