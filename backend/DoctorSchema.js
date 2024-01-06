83649cf9e77c7994c9159c35274da5898c97513e
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2024-01-07 00:04:30 +0300

    Update DoctorSchema.js

--- backend/DoctorSchema.js
+++ backend/DoctorSchema.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const DoctorSchema = new mongoose.Schema({
+
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
